85ddeb5fc354e036a012c89700b0988a46991c93
Silviu Eigel <silviu.eigel@thinslices.com>
2017-06-17 15:51:38 +0300

    add pangram solution

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -1,7 +1,26 @@
+//Package pangram offers methods for identifying pangram phrases.
 package pangram
+
+import "strings"
 
 const testVersion = 1
 
-func IsPangram(string) bool{
-	return false
+//IsPangram identifies if a phrase is a pangram - it contains all the letters of the alphabet.
+func IsPangram(phrase string) bool {
+	lowerCasePhrase := strings.ToLower(phrase)
+
+	for _, letter := range alphabetLetters() {
+		if !strings.Contains(lowerCasePhrase, string(letter)) {
+			return false
+		}
+	}
+	return true
+}
+
+func alphabetLetters() []rune {
+	var letters []rune
+	for i := 'a'; i < 'z'; i++ {
+		letters = append(letters, i)
+	}
+	return letters
 }
