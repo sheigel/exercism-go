22e11a2c0f7b0842ccb5b834959b6ce38a0c790a
Silviu Eigel <silviu.eigel@thinslices.com>
2017-06-18 00:20:12 +0300

    add bob solution

---
+++ bob/bob.go
@@ -0,0 +1,42 @@
+//Package bob simulates a lackadaisical teenager
+package bob
+
+import (
+	"strings"
+	"regexp"
+)
+
+const testVersion = 3
+
+//Hey function gives the answers of a lackadaisical teenager to a phrase
+func Hey(phrase string) string {
+	if yell(phrase) {
+		return "Whoa, chill out!"
+	}
+	if question(phrase) {
+		return "Sure."
+	}
+	if silence(phrase) {
+		return "Fine. Be that way!"
+	}
+	return "Whatever."
+}
+
+func yell(phrase string) bool {
+	everythingButLettersFinder, _ := regexp.Compile(`[^\w]|[0-9]`)
+
+	onlyCharacters := everythingButLettersFinder.ReplaceAllString(phrase, "")
+
+	return onlyCharacters == strings.ToUpper(onlyCharacters) && len(onlyCharacters) > 0
+}
+
+func silence(phrase string) bool {
+	nonSpaceFinder, _ := regexp.Compile(`[^\s]`)
+	return !nonSpaceFinder.MatchString(phrase)
+}
+
+func question(phrase string) bool {
+	questionMarkFinder, _ := regexp.Compile("[?][\\s]*$")
+	return questionMarkFinder.MatchString(phrase)
+
+}
