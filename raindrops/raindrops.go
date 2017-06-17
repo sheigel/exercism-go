1907d1fdea92732adf8279cab5ad19608dfafd68
Silviu Eigel <silviu.eigel@thinslices.com>
2017-06-17 11:59:17 +0300

    add raindrops solution

---
+++ raindrops/raindrops.go
@@ -0,0 +1,34 @@
+package raindrops
+
+import "strconv"
+
+const testVersion = 3
+
+// Converts the given integer to raindrop-speak
+func Convert(num int) string {
+	translation := pling(num) + plang(num) + plong(num)
+	if len(translation) > 0 {
+		return translation
+	}
+	return strconv.Itoa(num)
+}
+
+func pling(num int) string {
+	if num%3 == 0 {
+		return "Pling"
+	}
+	return ""
+}
+
+func plang(num int) string {
+	if num%5 == 0 {
+		return "Plang"
+	}
+	return ""
+}
+func plong(num int) string {
+	if num%7 == 0 {
+		return "Plong"
+	}
+	return ""
+}
