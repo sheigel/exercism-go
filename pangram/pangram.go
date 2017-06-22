e4472363c854ae5b5c405a9a95ebee409c39dc03
Silviu Eigel <silviu.eigel@thinslices.com>
2017-06-22 21:12:12 +0300

    refactor pangram algorithm to use strings.ContainsRune method instead Contains that uses strings
    As we generate the alphabet as a slice of runes there is no need to
    convert these to strings
    It makes the code more straightforward.

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -9,15 +9,15 @@ const testVersion = 1
 func IsPangram(phrase string) bool {
 	lowerCasePhrase := strings.ToLower(phrase)
 
-	for _, letter := range alphabetLetters() {
-		if !strings.Contains(lowerCasePhrase, string(letter)) {
+	for _, letter := range alphabet() {
+		if !strings.ContainsRune(lowerCasePhrase, letter) {
 			return false
 		}
 	}
 	return true
 }
 
-func alphabetLetters() []rune {
+func alphabet() []rune {
 	var letters []rune
 	for i := 'a'; i < 'z'; i++ {
 		letters = append(letters, i)
