4a9ac15390f90a47287023d0016ba01918fcee57
Silviu Eigel <silviu.eigel@thinslices.com>
2017-06-22 21:37:17 +0300

    add twelve days solution

--- twelve-days/twelve_days.go
+++ twelve-days/twelve_days.go
@@ -1,13 +1,32 @@
+// Package twelve offers functionality around the lyrics of the Twelve Days Of Christmas song.
 package twelve
 
-const testVersion = 4
+import "strings"
 
-type song struct{}
+const testVersion = 1
 
-func Song() string {
-	return "song{}"
+var verses = []string{
+	`On the first day of Christmas my true love gave to me, a Partridge in a Pear Tree.`,
+	`On the second day of Christmas my true love gave to me, two Turtle Doves, and a Partridge in a Pear Tree.`,
+	`On the third day of Christmas my true love gave to me, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.`,
+	`On the fourth day of Christmas my true love gave to me, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.`,
+	`On the fifth day of Christmas my true love gave to me, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.`,
+	`On the sixth day of Christmas my true love gave to me, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.`,
+	`On the seventh day of Christmas my true love gave to me, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.`,
+	`On the eighth day of Christmas my true love gave to me, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.`,
+	`On the ninth day of Christmas my true love gave to me, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.`,
+	`On the tenth day of Christmas my true love gave to me, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.`,
+	`On the eleventh day of Christmas my true love gave to me, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.`,
+	`On the twelfth day of Christmas my true love gave to me, twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.`,
+	``,
 }
 
+// Song gives back the complete Twelve Days Of Christmas song text
+func Song() string {
+	return strings.Join(verses, "\n")
+}
+
+// Verse given a verse number gives back that particular verse.
 func Verse(index int) string {
-	return ""
+	return verses[index-1]
 }
